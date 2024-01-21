d20a3f4321ee96d68fdf2e782065551596fc64a9
Rapha-Borges <raps_rnb@hotmail.com>
2024-01-21 19:13:57 -0300

    Update the OCI cluster create-kubeconfig command to utilize an API key

--- OCI/terraform/giropops-senhas/deploy.tf
+++ OCI/terraform/giropops-senhas/deploy.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "create_kubeconfig" {
   provisioner "local-exec" {
-    command = "oci ce cluster create-kubeconfig --cluster-id ${var.cluster_id} --file ~/.kube/config --token-version 2.0.0 --kube-endpoint PUBLIC_ENDPOINT --auth security_token"
+    command = "oci ce cluster create-kubeconfig --cluster-id ${var.cluster_id} --file ~/.kube/config --token-version 2.0.0 --kube-endpoint PUBLIC_ENDPOINT --auth api_key --region ${var.region} --profile ${var.oci_profile}"
   }
 }
 
