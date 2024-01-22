a4a065bf701bd08cef6a119b8cfe7d53a8f05ab3
Rapha-Borges <raps_rnb@hotmail.com>
2024-01-21 21:33:01 -0300

    Update private key path

--- OCI/terraform/sample.env.sh
+++ OCI/terraform/sample.env.sh
@@ -5,6 +5,6 @@ export TF_VAR_region=<region>
 export TF_VAR_config_file_profile=<config_file_profile>
 export TF_VAR_ssh_public_key=$(cat id_rsa.pub)
 export TF_VAR_ssh_private_key=$(cat id_rsa)
-export TF_VAR_private_key_path="terraform/private_key.pem"
+export TF_VAR_private_key_path="private_key.pem"
 # It must be the same as the one in OCI cofing file
 #export TF_VAR_oci_profile="<oci_profile>"
